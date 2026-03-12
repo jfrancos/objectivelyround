fcae95c637a72b5389e04d438180229bf4188da1
Justin Francos <justinfrancos@gmail.com>
2026-03-12 19:55:43 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const dist = (x: number) => {
+    while (x % 1 !== 0) x *= 2;
+    const result = 1 + Math.clz32(x & -x) - Math.clz32(x);
+    console.log(result);
+    return result;
+  };
+</script>
+
+<main class="h-dvh p-8">
+  {#each { length: 16 }, i}
+    <div style:padding-left={`${dist(i / 16) * 32}px`}>{i / 16}</div>
+  {/each}
+  {#each { length: 2048 }, i}
+    <div style:padding-left={`${dist(i + 1) * 32}px`}>{i + 1}</div>
+  {/each}
+</main>
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { presetWind4 } from "unocss";
+import unoCSS from "unocss/vite";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), unoCSS({ presets: [presetWind4()] })],
+});
