60e591a51e7d6acdcd5e4fd290b2a88b573aba5d
Justin Francos <justinfrancos@gmail.com>
2026-03-13 09:51:58 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
   import Fraction from "fraction.js";
 
-  const padding = (i: number) => `${dist(i) * 16}px`;
-  const fontSize = (i: number) => `${32 - dist(i) * 1.5}px`;
+  const padding = (i: number) => `${i * 16}px`;
+  const fontSize = (i: number) => `${32 - i * 1.5}px`;
 
   const dist = (x: number) => {
-    while (x % 1 !== 0) x *= 2;
+    while (!Number.isInteger(x)) x *= 2;
     return Math.clz32(x & -x) - Math.clz32(x);
   };
 </script>
 
 <main class="p-8 font-mono">
-  {#each Array.from({ length: 15 }, (_, x: number) => x + 1) as i}
-    <div style:padding-left={padding(i)} style:font-size={fontSize(i)}>
-      {new Fraction(i / 16).toFraction()}
+  {#each Array.from({ length: 16 }, (_, x: number) => x + 1) as i}
+    <div
+      style:padding-left={padding(dist(i))}
+      style:font-size={fontSize(dist(i))}
+    >
+      {new Fraction(i, 16).toFraction()}
     </div>
   {/each}
-  {#each Array.from({ length: 2 ** 16 }, (_, x: number) => x + 1).filter((n) => n % 2 === 0 || n === 1) as i}
-    <div style:padding-left={padding(i)} style:font-size={fontSize(i)}>
-      {i}
+  {#each Array.from( { length: 2 ** 14 }, (_, x: number) => ({ x: (x + 1) * 2, dist: dist((x + 1) * 2) }), ) as i}
+    <div
+      style:padding-left={padding(i.dist)}
+      style:font-size={fontSize(i.dist)}
+    >
+      {i.x}
     </div>
   {/each}
 </main>
