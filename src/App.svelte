06224e4cc7d345923a298b988107942a589faf4f
Justin Francos <justinfrancos@gmail.com>
2026-03-13 00:03:20 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+  import Fraction from "fraction.js";
+
   const dist = (x: number) => {
     while (x % 1 !== 0) x *= 2;
-    const result = 1 + Math.clz32(x & -x) - Math.clz32(x);
-    console.log(result);
-    return result;
+    return Math.clz32(x & -x) - Math.clz32(x);
   };
 </script>
 
-<main class="h-dvh p-8">
-  {#each { length: 16 }, i}
-    <div style:padding-left={`${dist(i / 16) * 32}px`}>{i / 16}</div>
+<main class="p-8">
+  {#each Array.from({ length: 15 }, (_, x: number) => x + 1) as i}
+    <div
+      style:padding-left={`${dist(i / 16) * 24}px`}
+      style:font-size={`${32 - dist(i) * 2}px`}
+    >
+      {new Fraction(i / 16).toFraction()}
+    </div>
   {/each}
-  {#each { length: 2048 }, i}
-    <div style:padding-left={`${dist(i + 1) * 32}px`}>{i + 1}</div>
+  {#each Array.from({ length: 4096 }, (_, x: number) => x + 1).filter((n) => n % 2 === 0 || n === 1) as i}
+    <div
+      style:padding-left={`${dist(i) * 24}px`}
+      style:font-size={`${32 - dist(i) * 2}px`}
+    >
+      {i}
+    </div>
   {/each}
 </main>
