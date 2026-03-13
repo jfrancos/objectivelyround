9149d896787dcf72cfcafb4f3315d1efe28a4ca9
Justin Francos <justinfrancos@gmail.com>
2026-03-13 00:23:25 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Fraction from "fraction.js";
 
+  const padding = (i: number) => `${dist(i) * 16}px`;
+  const fontSize = (i: number) => `${32 - dist(i) * 1.5}px`;
+
   const dist = (x: number) => {
     while (x % 1 !== 0) x *= 2;
     return Math.clz32(x & -x) - Math.clz32(x);
@@ -9,18 +12,12 @@
 
 <main class="p-8 font-mono">
   {#each Array.from({ length: 15 }, (_, x: number) => x + 1) as i}
-    <div
-      style:padding-left={`${dist(i / 16) * 24}px`}
-      style:font-size={`${32 - dist(i) * 2}px`}
-    >
+    <div style:padding-left={padding(i)} style:font-size={fontSize(i)}>
       {new Fraction(i / 16).toFraction()}
     </div>
   {/each}
   {#each Array.from({ length: 2 ** 16 }, (_, x: number) => x + 1).filter((n) => n % 2 === 0 || n === 1) as i}
-    <div
-      style:padding-left={`${dist(i) * 16}px`}
-      style:font-size={`${32 - dist(i) * 1.5}px`}
-    >
+    <div style:padding-left={padding(i)} style:font-size={fontSize(i)}>
       {i}
     </div>
   {/each}
