09d8d1ff993bf76e282fd17705aee8370dadcb55
Justin Francos <justinfrancos@gmail.com>
2026-03-21 15:35:02 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,18 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button";
-  import {
-    InputGroup,
-    InputGroupAddon,
-    InputGroupInput,
-  } from "$lib/components/ui/input-group";
+  import { onMount } from "svelte";
+  import { InputGroup, InputGroupInput } from "$lib/components/ui/input-group";
   import { Separator } from "$lib/components/ui/separator";
-  import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-  } from "$lib/components/ui/tooltip";
-  import { dist } from "./lib/utils";
-  import Popover from "./Popover.svelte";
-  import ScaleChooser from "./ScaleChooser.svelte";
+  import { getNeighbors } from "./lib/utils";
 
   let chosenDistance = $state<number>();
   let scale = $state(1);
-  let input = $state("");
+  let input = $state("1337");
   let inputRef = $state<HTMLInputElement | null>(null);
 
-  const integers = $derived(
-    Array.from({ length: 2 ** 13 }, (_, x: number) => ({
-      value: x + 1,
-      distance: dist(x + 1),
-    })).filter((item) =>
-      chosenDistance !== undefined
-        ? item.distance < chosenDistance || String(item.value / scale) === input
-        : true,
-    ),
-  );
+  const target = $derived(Number(input));
+  const neighbors = $derived(getNeighbors(target));
+
+  const EXP = $derived(Math.log2(neighbors[0].number) + 2);
 
   const scrollToInput = () => {
     const hasInput = input.trim() !== "";
@@ -46,6 +28,8 @@
     }
   };
 
+  onMount(() => inputRef?.focus());
+
   $effect(() => {
     void scale;
     void chosenDistance;
@@ -56,20 +40,6 @@
     void input;
     chosenDistance = undefined;
   });
-
-  const showEsc = $derived(chosenDistance !== undefined);
-  const showEnter = $derived(
-    integers.some((item) => String(item.value / scale) === input),
-  );
-
-  const focus = () => {
-    const number = integers.find(
-      (item) => String(item.value / scale) === input,
-    );
-    if (number != null) {
-      chosenDistance = number.distance;
-    }
-  };
 </script>
 
 <svelte:window
@@ -82,15 +52,12 @@
   }}
 />
 
-<main class="relative">
-  <header
-    class="sticky top-0 z-10 shadow-lg bg-white w-full py-6 px-8 flex justify-between z-20"
-  >
-    <InputGroup class="w-40">
+<main class="relative w-full  overflow-visible">
+  <header class="sticky top-0 shadow-lg bg-white p-8 flex justify-center z-20">
+    <InputGroup class="w-48">
       <InputGroupInput
         bind:ref={inputRef}
         placeholder="Target"
-        autofocus
         bind:value={
           () => input,
           (next) => {
@@ -105,93 +72,47 @@
           }
         }}
       />
-      {#if showEsc || showEnter}
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <InputGroupAddon align="inline-end">
-                <Button
-                  size="sm"
-                  onclick={() => {
-                    console.log("click");
-                    if (showEsc) {
-                      chosenDistance = undefined;
-                    } else if (showEnter) {
-                      focus();
-                    }
-                  }}
-                  class="bg-neutral-600 text-neutral-50 rounded text-xs h-6"
-                >
-                  {#if showEsc}
-                    <span class="leading-0">Esc</span>
-                  {:else if showEnter}
-                    <span class="-mb-0.5">↵</span>
-                  {/if}
-                </Button>
-              </InputGroupAddon>
-            </TooltipTrigger>
-            <TooltipContent>
-              {#if showEsc}
-                Show all values
-              {:else if showEnter}
-                Focus on <span class="font-bold">only</span> rounder values
-              {/if}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      {/if}
     </InputGroup>
-    <ScaleChooser bind:scale />
-    <div class="w-40 flex justify-end">
-      <Popover />
-    </div>
   </header>
-  <div class="sticky h-0 top-23 -ml-3 mr-3 font-mono text-sm">
-    {#each { length: 13 }, i}
-      <div class="absolute px-16 inset-0">
-        <div class="w-4 text-end" style={`margin-left: ${i * 8}%`}>
-          {i}
+  <div class="sticky h-0 top-23 .-ml-3 .mr-3 font-mono text-sm">
+    {#each { length: EXP }, i}
+      <div class="absolute .px-16 inset-0">
+        <div class="w-4 text-end" style={`margin-left: ${(i / EXP) * 100}%`}>
+          {EXP - i - 1}
         </div>
       </div>
     {/each}
   </div>
 
-  <div class="font-mono py-8 color-neutral-800 bg-[oklch(97.5%.075_300)]">
-    <div class=".max-w-80">
-      {#each { length: 13 }, i}
-        <div class="absolute px-16 inset-0 inset-l-2 -inset-r-2">
-          <Separator
-            orientation="vertical"
-            class="bg-neutral-500/50"
-            style={`margin-left: ${i * 8}%`}
-          />
-        </div>
-      {/each}
-      {#each integers as { value, distance }}
+  <div
+    class="font-mono py-8 color-neutral-800 .bg-[oklch(97.5%.075_300)] w-full overflow-visible"
+  >
+    <!-- {#each { length: EXP }, i}
+      <div class="absolute .px-16 inset-0 .inset-l-2 .-inset-r-2">
+        <Separator
+          orientation="vertical"
+          class="bg-neutral-500/50"
+          style={`margin-left: ${(i / EXP) * 100}%`}
+        />
+      </div>
+    {/each} -->
+    {#each neighbors as { number, timesDivisibleBy2: distance }}
+      <div
+        class="w-full overflow-visible"
+        style:background-color={`oklch(${97.5 - 2.5 * (EXP - distance - 1)}% 0.075 300)`}
+        style:padding-left={`${((EXP - distance - 1) / EXP) * 100}%`}
+      >
         <div
-          class="flex items-center px-16"
-          style:background-color={`oklch(${97.5 - 2.5 * distance}% 0.075 300)`}
+          class={[
+            "value .size-fit px-2 relative .mx-16 text-xl w-full overflow-visible",
+            // input &&
+            //   String(number / scale).startsWith(input.trim()) &&
+            //   "bg-yellow-300",
+          ]}
         >
-          <!-- <div class="absolute w-16 p-2 shrink-0 left-0">
-            {distance}
-          </div> -->
-          <div
-            style:padding-left={`${distance * 8}%`}
-            style:font-size={`${40 - distance * 2}px`}
-          >
-            <div
-              class={[
-                "value size-full px-2 z-10 relative",
-                input &&
-                  String(value / scale).startsWith(input.trim()) &&
-                  "bg-yellow-300",
-              ]}
-            >
-              {value / scale}
-            </div>
-          </div>
+          {number / scale}
         </div>
-      {/each}
-    </div>
+      </div>
+    {/each}
   </div>
 </main>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,5 +15,63 @@ export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
 	ref?: U | null;
 };
+type Item = {
+	number: number;
+	timesDivisibleBy2: number;
+};
 
-export const dist = (x: number) => Math.clz32(x & -x) - Math.clz32(x);
+const timesDivisibleBy2 = (x: number) => {
+	let count = 0;
+	for (let n = x; n % 2 === 0; n /= 2) count++;
+	return count;
+};
+
+const isPowerOf2 = (x: number) => {
+	if (x < 1 || !Number.isInteger(x)) return false;
+	while (x % 2 === 0) x /= 2;
+	return x === 1;
+};
+
+const findNext = (
+	start: number,
+	step: -1 | 1,
+	min: number,
+): Item | undefined => {
+	const factor = 2 ** min;
+
+	const number =
+		step === 1
+			? Math.ceil(start / factor) * factor
+			: Math.floor(start / factor) * factor;
+
+	if (number < 1) return;
+
+	return {
+		number,
+		timesDivisibleBy2: min + timesDivisibleBy2(number / factor),
+	};
+};
+
+export const getNeighbors = (target: number): Item[] => {
+	const middle = {
+		number: target,
+		timesDivisibleBy2: timesDivisibleBy2(target),
+	};
+
+	const walk = (step: -1 | 1) => {
+		const items: Item[] = [];
+		let min = middle.timesDivisibleBy2 + 1;
+		let start = target + step;
+
+		while (true) {
+			const next = findNext(start, step, min);
+			if (!next) return items;
+			items[step === -1 ? "unshift" : "push"](next);
+			if (isPowerOf2(next.number)) return items;
+			min = next.timesDivisibleBy2 + 1;
+			start = next.number + step;
+		}
+	};
+
+	return [...walk(-1), middle, ...walk(1)];
+};
