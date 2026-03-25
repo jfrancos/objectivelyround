1b792685974fe33535f331e8b397f0b9e26df213
Justin Francos <justinfrancos@gmail.com>
2026-03-24 22:30:16 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,88 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { InputGroup, InputGroupInput } from "$lib/components/ui/input-group";
+  import { Input } from "$lib/components/ui/input";
   import { getNeighbors } from "./lib/utils";
 
-  let input = $state("1337");
+  let input = $state(1337);
   let inputRef = $state<HTMLInputElement | null>(null);
-
-  const target = $derived(Number(input));
-  const neighbors = $derived(getNeighbors(target));
-
-  const EXP = $derived(Math.log2(neighbors[0].number) + 2);
-
+  let showLimit = $state(false);
+  const neighbors = $derived(input ? getNeighbors(input) : []);
   onMount(() => inputRef?.focus());
 
-  $inspect(neighbors);
+  $inspect(input);
 </script>
 
 <svelte:window
   onkeydown={({ key }) => {
-    if (input === "" && key.match(/[\d]/)) {
+    if (input === null && key.match(/[\d]/)) {
       inputRef?.focus();
     }
   }}
 />
 
-<main class=".relative .w-full .overflow-visible">
-  <header class="sticky top-0 shadow-lg bg-white p-8 flex justify-center z-20">
-    <InputGroup class="w-48">
-      <InputGroupInput
-        type="number"
-        bind:ref={inputRef}
-        placeholder="Target"
-        bind:value={
-          () => input,
-          (next) => {
-            if (Number.isFinite(Number(next)) && next.trim() === next) {
-              input = next;
-            }
-          }
-        }
-        onkeydown={({ key }) => {
-          if (key === "Enter") {
-            focus();
-          }
-        }}
-      />
-    </InputGroup>
-  </header>
-  <div class=".flex .flex-col">
-    {#each neighbors as { number, exponent, delta, factor, rank }}
-      <div
-        class="px-4"
-        style:background-color={`oklch(${1 - (0.375 * rank) / (neighbors.length - 1)} 0.1 300)`}
-      >
-        <div
-          class="text-right py-2 relative w-fit"
-          style:left={`${(100 * rank) / (neighbors.length - 1)}%`}
-          style:transform={`translateX(${(-100 * rank) / (neighbors.length - 1)}%)`}
-        >
-          <div class="flex items-end flex-col w-fit min-w-0">
-            <math class="text-neutral-700/75 text-sm font-sans">
-              <mn>{factor}</mn>
-              <mo>&times;</mo>
-              <msup>
-                <mn>2</mn>
-                <!-- do 2^(0 + 6) -->
-                <mn class="font-semibold text-black text-11px">{exponent}</mn>
-              </msup>
-            </math>
-            <div class={["text-xl font-mono color-neutral-800"]}>
-              {number}
-            </div>
-            <math class="font-sans text-11px">
-              <mo class="text-neutral-700/75 relative -top-[0.125em]">
-                {delta > 0 ? "+" : delta < 0 ? "−" : ""}
-              </mo>
-              <mn class="font-semibold text-black">
-                {Math.abs(delta)}
-              </mn>
-            </math>
-          </div>
-        </div>
-      </div>
-    {/each}
+<header
+  class="sticky top-0 shadow-lg py-2 bg-white flex justify-center z-10 flex flex-col items-center gap-2 text-xs font-medium"
+>
+  <div class={[showLimit || "invisible"]}>
+    You’ve reached the artificial limit of <math class="font-sans font-medium"
+      ><msup><mn>2</mn><mn>48</mn></msup></math
+    >
   </div>
-</main>
+  <Input
+    max={16}
+    class="w-48"
+    type="number"
+    placeholder="Target"
+    bind:ref={inputRef}
+    bind:value={
+      () => input,
+      (next) => {
+        if (next < 2 ** 48) {
+          input = next;
+          showLimit = false;
+        } else {
+          showLimit = true;
+        }
+      }
+    }
+  />
+  <a
+    target="_blank"
+    class={["underline color-purple-700", showLimit || "invisible"]}
+    href="https://github.com/jfrancos/objectivelyround/issues"
+    >Tell me about your use case</a
+  >
+</header>
+{#each neighbors as { number, exponent, delta, factor, rank }}
+  {@const percentage = rank / (neighbors.length - 1)}
+  <div
+    class="px-4 h-18 flex items-center"
+    style:background-color={`oklch(${1 - 0.375 * percentage} 0.1 300)`}
+  >
+    <div
+      class="relative flex items-end flex-col"
+      style:left={`${100 * percentage}%`}
+      style:transform={`translateX(${-100 * percentage}%)`}
+    >
+      <math class="text-neutral-600 text-sm font-sans">
+        <mn>{factor}</mn>
+        <mo>&times;</mo>
+        <msup>
+          <mn>2</mn>
+          <mn class="font-semibold text-black text-11px">{exponent}</mn>
+        </msup>
+      </math>
+      <div class="text-xl font-mono color-neutral-800">
+        {number}
+      </div>
+      <math class="font-sans text-11px">
+        <mo class="text-neutral-600 relative -top-[0.125em]">
+          {delta > 0 ? "+" : delta < 0 ? "−" : ""}
+        </mo>
+        <mn class="font-semibold text-black">
+          {Math.abs(delta)}
+        </mn>
+      </math>
+    </div>
+  </div>
+{/each}
